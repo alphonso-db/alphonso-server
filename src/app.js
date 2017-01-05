4fd6b9b4696692755cf9e77715d7b59ea456e95f
hegdeashwin <ashwin.hegde3@gmail.com>
2017-01-06 00:52:23 +0530

    Integrate Koa2

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import path from 'path';
-import koa from 'koa';
+import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import logger from 'koa-logger';
-import staticServe from 'koa-static';
+// import staticServe from 'koa-static';
 import CONFIG from '../configs/stack.conf';
 
-let app = koa();
+const app = new Koa();
 
 app.use(bodyParser());
 app.use(logger());
 
-app.use(staticServe(path.join(__dirname, CONFIG.stack.client)));
+// app.use(staticServe(path.join(__dirname, CONFIG.stack.client)));
 
 module.exports = app;
