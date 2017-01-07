816f76dc87665a34d853f084bb920a710e4a9b56
hegdeashwin <ashwin.hegde3@gmail.com>
2017-01-07 18:28:48 +0530

    Add/Update packages

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
-import path from 'path';
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import logger from 'koa-logger';
-// import staticServe from 'koa-static';
-import CONFIG from '../configs/stack.conf';
+import helmet from 'koa-helmet';
 
 const app = new Koa();
 
 app.use(bodyParser());
 app.use(logger());
-
-// app.use(staticServe(path.join(__dirname, CONFIG.stack.client)));
+app.use(helmet());
 
 module.exports = app;
