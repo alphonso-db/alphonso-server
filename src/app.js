a984edb16a01ce4429db96f39f333b1b6cb722b5
hegdeashwin <ashwin.hegde3@gmail.com>
2017-01-07 19:59:02 +0530

    Integrate packages

--- src/app.js
+++ src/app.js
@@ -2,11 +2,31 @@ import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import logger from 'koa-logger';
 import helmet from 'koa-helmet';
+import json from 'koa-json';
+import onerror from 'koa-onerror';
+import router from 'koa-router';
+import convert from 'koa-convert';
 
 const app = new Koa();
 
-app.use(bodyParser());
-app.use(logger());
-app.use(helmet());
+app.use(convert(bodyParser));
+app.use(convert(json()));
+app.use(convert(logger()));
+app.use(convert(helmet()));
+app.use(router.routes(), routes.allowedMethods());
+
+app.use(async (ctx, next) => {
+	const start = new Date();
+	await next();
+
+	const ms = new Date() - start;
+	console.info(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+router.use('/api/v1/connection', require('./routes/v1/connect/connect.api'));
+
+app.on('error', (error, ctx) => {
+	logger.error('server error', error, ctx);
+});
 
 module.exports = app;
